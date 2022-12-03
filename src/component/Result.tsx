2b24fe8cca8a3bc5532d42aa7e3fd7f3f4b86493
Anantha Madhava Charya A <45356992+AnanthaMadhava@users.noreply.github.com>
2022-12-03 07:43:48 +0530

    Update Result.tsx
    Updated

--- src/component/Result.tsx
+++ src/component/Result.tsx
@@ -2,28 +2,7 @@ import React, { useContext } from 'react';
 import { QuestionContext } from '../contexts/QuestionContext';
 
 const Result = () => {
-    const { questions }: any = useContext(QuestionContext);
-    
-    const scorecount = (data: any) => {
-        for(let j=0; j<=data.length - 1; j++) {
-            if(data[j].iscorrect === data[j].selected) {
-                return true;
-            } else {
-                return false
-            }
-        }
-    }
-
-    const score = () => {
-        let result = 0;
-        for(let i=0 ; i<=questions.length - 1; i++) {
-            let obj = questions[i]?.answerOptions;
-            if(scorecount(obj) === true) {
-                result += 1
-            }
-        }
-        return result;
-    }
+    const { questions, score }: any = useContext(QuestionContext);
     
     const ans = questions.map((list: any) => {
         return {
@@ -34,7 +13,7 @@ const Result = () => {
 
     return (
         <div className='score-container'>
-            <h1>You scored {score()} out of {questions.length}</h1>
+            <h1>You scored {score} out of {questions.length}</h1>
         </div>
     );
 };
